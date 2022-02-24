12b81923fc8a0d78d572149453f130e450cf4cda
Makoto87 <makosinhori@gmail.com>
2022-02-24 22:31:05 +0900

    add http server

--- app/controllers/webserver.go
+++ app/controllers/webserver.go
@@ -1,2 +1,20 @@
 package controllers
 
+import (
+	"html/template"
+	"net/http"
+)
+
+// メイン画面の表示
+var templates = template.Must(template.ParseFiles("app/views/main.html"))
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	if err := templates.ExecuteTemplate(w, "main.html", nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func StartWebServer() error {
+	http.HandleFunc("/view/", viewHandler)
+	return http.ListenAndServe(":8080", nil)
+}
