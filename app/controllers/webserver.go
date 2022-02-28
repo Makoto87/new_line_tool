71cfeba1c843d5a507648db7817bb7af88730516
Makoto87 <makosinhori@gmail.com>
2022-02-28 22:15:31 +0900

    css, jsファイルをhtmlに反映

--- app/controllers/webserver.go
+++ app/controllers/webserver.go
@@ -1,8 +1,12 @@
 package controllers
 
 import (
+	"encoding/json"
+	"fmt"
 	"html/template"
+	"io"
 	"net/http"
+	"os"
 )
 
 // メイン画面の表示
@@ -14,7 +18,89 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// レスポンスで返す用のJSON
+type Newline struct {
+	// Status int  `json:"status"`
+	Text string `json:"text"`
+}
+
+// 改行を行う
+func newLineHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("request arrived")
+	// POSTじゃなかったらエラー
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// josnじゃなかったらエラー
+	if r.Header.Get("Content-Type") != "application/json" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// Bodyを読み込み
+	body, err := io.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil && err != io.EOF {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// jsonを読み込み
+	// decoderとどっちを使えばいい？
+	var newLine Newline
+	if err = json.Unmarshal(body, &newLine); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	// decoerの場合
+	// json.NewDecoder(r.Body).Decode(&newLine)
+
+	// 構造体をjson化
+	jsondata, err := json.Marshal(newLine)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// headerのセット
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	// w.WriteHeader(http.StatusOK)
+
+	// bodyのセット
+	w.Write(jsondata)
+	// fmt.Fprint(w, string(jsondata))
+}
+
+// test
+// func testHandler(w http.ResponseWriter, r *http.Request) {
+// 	//構造体をjson化
+// 	newLine := Newline{"text"}
+// 	jsondata, err := json.Marshal(newLine)
+// 	if err != nil {
+// 		w.WriteHeader(http.StatusInternalServerError)
+// 		return
+// 	}
+
+// 	// headerのセット
+// 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+// 	// w.WriteHeader(http.StatusOK)
+
+// 	// bodyのセット
+// 	w.Write(jsondata)
+// 	// fmt.Fprint(w, string(jsondata))
+// }
+
 func StartWebServer() error {
 	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/newline/", newLineHandler)
+
+	// cssやjsを読め込めるようにする
+	dir, _ := os.Getwd()
+	// ディレクトリを指定して公開
+	http.Handle("/app/views/", http.StripPrefix("/app/views/", http.FileServer(http.Dir(dir+"/app/views/"))))
+
+
 	return http.ListenAndServe(":8080", nil)
 }
--- app/views/scripts/senddata.js
+++ app/views/scripts/senddata.js
@@ -2,21 +2,62 @@
 
 // inputのデータを取得
 // 取得したデータをサーバーへリクエスト
-function createNewLine() {
+// function test() {
 
-      let url = "https://weather.tsukumijima.net/api/forecast";
-      fetch(url + "?city=410020").then(function(response) {
-            console.log(response);
-            if (response.ok) {
-                  response.json().then(function(json) {
-                        console.log(json.publicTime);
-                        outputId.value = json.publicTime;
-                  })
-                  .catch(e => {
-                        console.log(e.message);
-                  });
-            } else {
-                  outputId.value = "fatal";
-            }
-      });
+//       let url = "http://localhost/newline";
+//       fetch(url).then(function(response) {
+//             console.log(response);
+//             if (response.ok) {
+//                   await response.json().then(function(json) {
+//                         console.log(json.publicTime);
+//                         outputId.value = json.publicTime;
+//                   })
+//                   .catch(e => {
+//                         console.log(e.message);
+//                   });
+//             } else {
+//                   outputId.value = "fatal";
+//             }
+//       });
+// }
+
+async function getNewLine(url = '', data = {}) {
+      const response = await fetch(url, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            redirect: 'error',
+            referrerPolicy: 'no-referrer',
+            body: JSON.stringify(data)
+      })
+      return response.json();
 }
+
+function changeLine() {
+      // console.log(typeof(inputId.value))
+      const url = '/newline/';
+      console.log(url);
+      let data = {text: inputId.value};
+      getNewLine(url, data).then(data => {
+            console.log(data);
+            outputId.value = data.text;
+      })
+}
+
+// function createNewLine() {
+
+//       let url = "https://weather.tsukumijima.net/api/forecast";
+//       fetch(url + "?city=410020").then(function(response) {
+//             console.log(response);
+//             if (response.ok) {
+//                   response.text().then(function(text) {
+//                         console.log(text);
+//                         outputId.value = text;
+//                   })
+//                   .catch(e => {
+//                         console.log(e.message);
+//                   });
+//             } else {
+//                   outputId.value = "fatal";
+//             }
+//       });
+// }
